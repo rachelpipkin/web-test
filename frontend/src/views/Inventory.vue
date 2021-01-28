8ccc81fab9da177d0d1d129881c337134212b210
Rachel Pipkin <rachelpipkin@gmail.com>
2021-01-27 21:42:32 -0800

    adds lodash and date filter on inventory list

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -18,19 +18,58 @@
     </div>
     <inventory-create v-if="isActive('create')" />
     <div v-else>
-      inventory table here
+      <date-picker @change="updateDate" />
+      <div class="flex-column" id="inventory-table">
+        <table>
+          <tr>
+            <th>Start</th>
+            <th>End</th>
+            <th>Booked</th>
+            <th>Total</th>
+          </tr>
+          <tr v-for="inv in filtedInventoryList" :key="inv.id">
+            <td>{{ inv.startTime }}</td>
+            <td>{{ inv.endTime }}</td>
+            <td>{{ inv.booked }}</td>
+            <td>{{ inv.total }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import _filter from 'lodash/filter'
 import InventoryCreate from './InventoryCreate'
+import DatePicker from '../components/DatePicker'
+
 export default {
   name: 'Inventory',
-  components: { InventoryCreate },
+  components: { DatePicker, InventoryCreate },
   data() {
     return {
-      activeItem: 'view'
+      activeItem: 'view',
+      filterDate: '2021-01-29',
+      inventoryList: [
+        {
+          id: 1,
+          date: '2021-01-28',
+          startTime: '9:00am',
+          endTime: '9:15am',
+          booked: 2,
+          total: 5
+        }
+      ]
+    }
+  },
+  computed: {
+    filtedInventoryList: {
+      get() {
+        return _filter(this.inventoryList, i => {
+          return i.date == this.filterDate
+        })
+      }
     }
   },
   methods: {
@@ -39,7 +78,18 @@ export default {
     },
     setActive(menuItem) {
       this.activeItem = menuItem
+    },
+    updateDate(newVal) {
+      this.filterDate = newVal
     }
   }
 }
 </script>
+
+<style>
+#inventory-table {
+  margin: 0 auto;
+  text-align: left;
+  width: 50%;
+}
+</style>
